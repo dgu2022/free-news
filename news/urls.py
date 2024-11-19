519785be333b5a49c0511982ad0623ca2ce91190
dgu2022 <ccccavin12345@naver.com>
2024-11-19 14:07:35 +0900

    sixth commit

--- news/urls.py
+++ news/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('/404', views.error, name='error'),
+    path('/contact', views.contact, name='contact'),
+    path('/detail-page', views.detail-page, name='detail-page'),
+    path('/index', views.index, name='index'),
 ]
--- news/views.py
+++ news/views.py
@@ -5,3 +5,15 @@ from .models import Post
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'news/post_list.html', {'posts': posts})
+
+def error(request):
+    return render(request, 'news/error.html', {})
+
+def index(request):
+    return render(request, 'news/index.html', {})
+
+def contact(request):
+    return render(request, 'news/contact.html', {})
+
+def detail_page(request):
+    return render(request, 'news/detail-page.html', {})
