ee8bfc67d234f81d4057ef911abbd58b0d2cc946
dgu2022 <ccccavin12345@naver.com>
2024-11-19 14:09:38 +0900

    seventh commit

--- news/urls.py
+++ news/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('/404', views.error, name='error'),
     path('/contact', views.contact, name='contact'),
-    path('/detail-page', views.detail-page, name='detail-page'),
+    path('/detail-page', views.detail_page, name='detail-page'),
     path('/index', views.index, name='index'),
 ]
